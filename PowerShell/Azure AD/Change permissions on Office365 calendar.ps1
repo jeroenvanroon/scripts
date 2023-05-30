aeadc45424e62b39c9e47440bebbb4aab8c48d84
Jeroen van Roon <jeroen@antsec.nl>
2023-05-30 23:30:24 +0200

    Adding Get info specific mailbox

--- PowerShell/Azure AD/Change permissions on Office365 calendar.ps1
+++ PowerShell/Azure AD/Change permissions on Office365 calendar.ps1
@@ -4,6 +4,9 @@ Connect-ExchangeOnline -UserPrincipalName user.name@tenant.com
 #Get all mailboxes
 Get-mailbox
 
+#Get info specific mailbox
+Get-Mailbox -Identity "user.name" | Format-List
+
 #Get current permissions for specific user
 Get-MailboxFolderPermission user.name:\Calendar
 
